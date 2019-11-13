ee74c6ec4175bd25cc99723edbc5f302cdd6641a
TWESIGOMWE GILBERT <gilbert.gillz@gmail.com>
2019-11-13 11:48:43 +0300

    Initial Commit

---
+++ src/awamomarsroverkata/AwamoMarsRoverKata.java
@@ -0,0 +1,16 @@
+package awamomarsroverkata;
+
+/**
+ *
+ * @author GILBERT
+ */
+public class AwamoMarsRoverKata {
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
