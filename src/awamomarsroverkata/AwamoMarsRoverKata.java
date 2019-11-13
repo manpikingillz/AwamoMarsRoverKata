345200a2baa32f96c43631150c4c0decf6ba1469
TWESIGOMWE GILBERT <gilbert.gillz@gmail.com>
2019-11-13 11:51:34 +0300

    Added constructor to set starting point and initial direction

--- src/awamomarsroverkata/AwamoMarsRoverKata.java
+++ src/awamomarsroverkata/AwamoMarsRoverKata.java
@@ -5,8 +5,18 @@ package awamomarsroverkata;
  * @author GILBERT
  */
 public class AwamoMarsRoverKata {
-   
-
+    
+    int positionX = 0;
+    int positionY = 0;
+    char direction = 'N';
+    
+    //Rover constructor that sets the starting point (x,y) and initial direction
+    AwamoMarsRoverKata(int initialPositionX, int initialPositionY, char initialDirection){
+        positionX = initialPositionX;
+        positionY = initialPositionY;
+        direction = initialDirection;
+    }
+    
     /**
      * @param args the command line arguments
      */
